d964a1ec5207d9ede25338f98dc1f31628525709
ESSY555 <adewumiisrael66@gmail.com>
2025-01-27 15:05:46 +0100

    updated post propert page by adding image uploading section.

--- src/post_propert.vue
+++ src/post_propert.vue
@@ -52,47 +52,133 @@
       </div>
     </div>
 
-    <div class="flex justify-center pt-12">
-     <div>
-      <p class="font-bold text-center">Step 1: upload Property Documents</p>
-      <p class="text-gray-500">Please Upload relevant property documents (deeds,Certificates etc)</p>
-     </div>
+   
+    <div class="flex justify-center pt-12 border-b">
+  <!-- Step 1: Upload Property Documents -->
+  <div>
+    <p class="font-bold text-center">Step 1: Upload Property Documents</p>
+    <p class="text-gray-500 pt-4">Please upload relevant property documents (deeds, certificates, etc).</p>
 
-     <div>
-
-     </div>
+    <div class="bg-gray-50 mt-8 border flex justify-center p-8">
+      <!-- Image preview or upload prompt -->
+      <div @click="triggerFileInput1" class="cursor-pointer">
+        <img
+          v-if="uploadedImageUrl1"
+          :src="uploadedImageUrl1"
+          alt="Uploaded preview"
+          class="w-40 h-40 object-cover rounded border"
+        />
+        <div v-else class="flex flex-col items-center">
+          <img
+            src="../src/assets/images/plus.PNG"
+            alt="Upload icon"
+            class="w-12 h-12"
+          />
+          <p class="text-gray-500">Click to upload property documents</p>
+        </div>
+      </div>
+      <!-- Hidden file input -->
+      <input
+        type="file"
+        ref="fileInput1"
+        class="hidden"
+        accept="image/*"
+        @change="handleFileUpload1"
+      />
     </div>
+  </div>
+</div>
+
+<div class="flex justify-center pt-12">
+  <!-- Step 2: Upload Property Environment Photo -->
+  <div>
+    <p class="font-bold text-center">Step 2: Upload Property Environment Photo</p>
+    <p class="text-gray-500 pt-4">Add a clear photo showing the property's immediate surroundings.</p>
+
+    <div class="bg-gray-50 mt-8 border flex justify-center p-8">
+      <!-- Image preview or upload prompt -->
+      <div @click="triggerFileInput2" class="cursor-pointer">
+        <img
+          v-if="uploadedImageUrl2"
+          :src="uploadedImageUrl2"
+          alt="Uploaded preview"
+          class="w-40 h-40 object-cover rounded border"
+        />
+        <div v-else class="flex flex-col items-center">
+          <img
+            src="../src/assets/images/plus.PNG"
+            alt="Upload icon"
+            class="w-12 h-12"
+          />
+          <p class="text-gray-500">Click to upload environment photo</p>
+        </div>
+      </div>
+      <!-- Hidden file input -->
+      <input
+        type="file"
+        ref="fileInput2"
+        class="hidden"
+        accept="image/*"
+        @change="handleFileUpload2"
+      />
+    </div>
+  </div>
+</div>
+
   </div>
 </template>
 
 <script>
-import states from './states.json';  
+import states from './states.json'; 
 
 export default {
   name: 'PostProperty',
   data() {
     return {
-      states: states,  
-      selectedState: '',  
-      localGovernmentAreas: []  
+      states: states, 
+      selectedState: '', 
+      localGovernmentAreas: [], 
+      uploadedImageUrl1: null, 
+      uploadedImageUrl2: null, 
     };
   },
   methods: {
-   
+
     onStateChange() {
-    
       this.localGovernmentAreas = this.getLGAsForState(this.selectedState);
     },
 
- 
+
     getLGAsForState(stateId) {
-    
-      const state = this.states.find(state => state.id === stateId);
-      
+      const state = this.states.find((state) => state.id === stateId); 
+      return state ? state.lgas : []; 
+    },
+
+
+     triggerFileInput1() {
+      this.$refs.fileInput1.click();
+    },
  
-      return state ? state.lgas : [];
-    }
-  }
+    handleFileUpload1(event) {
+      const file = event.target.files[0];
+      if (file) {
+        this.uploadedImageUrl1 = URL.createObjectURL(file);
+        console.log('File selected (Step 1):', file.name);
+      }
+    },
+
+    triggerFileInput2() {
+      this.$refs.fileInput2.click();
+    },
+  
+    handleFileUpload2(event) {
+      const file = event.target.files[0];
+      if (file) {
+        this.uploadedImageUrl2 = URL.createObjectURL(file);
+        console.log('File selected (Step 2):', file.name);
+      }
+    },
+  },
 };
 </script>
 
