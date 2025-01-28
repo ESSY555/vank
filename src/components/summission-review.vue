1ebbd9e12a117176082da4d582cc3d0a3d40a0cf
ESSY555 <adewumiisrael66@gmail.com>
2025-01-28 15:51:08 +0100

    updatedd summission review page

--- src/components/summission-review.vue
+++ src/components/summission-review.vue
@@ -6,6 +6,9 @@
   
  <div class="flex justify-center h-screen">
     <div class="w-5/12 m-auto bg-white p-12 rounded-lg">
+        <div class="flex justify-center pb-5">
+        <img src="../assets/images/vank.PNG" alt="" />
+      </div>
 <p class="text-center font-bold text-3xl pb-3">Summission Review</p>
 <p class="text-gray-500">Your summission is being reviewed and subject
     to due diligentce. You will be contacted within 5 working days to begin due diligentce
